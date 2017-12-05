c82ed5c960d43437f8d45c7743fc4e4a3bd9221a
Jeff Knaggs <jknaggs@cfenet.ubc.ca>
2017-12-04 17:27:01 -0800

    test Mutation object seperately from 'Mutations' helper

--- pyvdrm/tests/test_vcf.py
+++ pyvdrm/tests/test_vcf.py
@@ -7,21 +7,20 @@ class TestMutationParser(unittest.TestCase):
         map(Mutation.from_string, "20N 10NNN 1ASDF 0X".split())
 
     def test_eq(self):
-        r1 = p.Mutation.from_string("20ASDF")
-        r2 = p.Mutation.from_string("20A")
+        r1 = Mutation.from_string("20A")
+        r2 = Mutation.from_string("20A")
         self.assertEqual(r2, r1)
 
     def test_ineq_pos(self):
-        r1 = Mutation.from_string("20ASDF")
+        r1 = Mutation.from_string("20A")
         r2 = Mutation.from_string("21A")
         self.assertNotEqual(r2, r1)
 
     def test_ineq_aa(self):
-        r1 = Mutation.from_string("20ASDF")
+        r1 = Mutation.from_string("20A")
         r2 = Mutation.from_string("20C")
         self.assertNotEqual(r2, r1)
 
 
 if __name__ == '__main__':
     unittest.main()
-        
