8445522059ac764e3eedc35c8c76bdb4a8644c96
jeff-k <jeff@x-0.ca>
2018-03-20 11:22:51 -0700

    first pass of working semantics

--- pyvdrm/hcvr2.py
+++ pyvdrm/hcvr2.py
@@ -11,166 +11,46 @@ from pyvdrm.drm import AsiExpr, AsiBinaryExpr, DRMParser
 from pyvdrm.vcf import MutationSet
 
 
-def update_flags(fst, snd):
-    for k in snd:
-        if k in fst:
-            fst[k].append(snd[k])
-        else:
-            fst[k] = snd[k]  # this could be achieved with a defaultdict
-    return fst
-
-
-@total_ordering
-class Score(object):
-    """Encapsulate a score and the residues that support it"""
-
-    def __init__(self, score, residues, flags=None):
-        """ Initialize.
-
-        :param bool|float score: value of the score
-        :param residues: sequence of Mutations
-        :param flags: dictionary of user defined strings and supporting Mutations
-        """
-        self.score = score
-        self.residues = set(residues)
-
-        if flags is None:
-            self.flags = {}
-        else:
-            self.flags = flags
-
-    def __add__(self, other):
-        flags = update_flags(self.flags, other.flags)
-        return Score(self.score + other.score,
-                     self.residues | other.residues,
-                     flags)
-
-    def __sub__(self, other):
-        flags = update_flags(self.flags, other.flags)
-        return Score(self.score - other.score,
-                     self.residues | other.residues,
-                     flags)
-
-    def __repr__(self):
-        return "Score({!r}, {!r})".format(self.score, self.residues)
-
-    def __eq__(self, other):
-        return self.score == other.score
-
-    def __lt__(self, other):
-        # the total_ordering decorator populates the other 5 comparison
-        # operations. Implement them explicitly if this causes performance
-        # issues
-        return self.score < other.score
-
-    def __bool__(self):
-        return self.score
-
-
 class BoolTrue(AsiExpr):
     """Boolean True constant"""
     def __call__(self, *args):
-        return Score(True, [])
+        return True
 
 
 class BoolFalse(AsiExpr):
     """Boolean False constant"""
     def __call__(self, *args):
-        return Score(False, [])
+        return False
 
 
-class AndExpr(AsiExpr):
-    """Fold boolean AND on children"""
+class AndExpr(AsiBinaryExpr):
+    """Boolean AND on children"""
 
     def __call__(self, mutations):
-        scores = map(lambda f: f(mutations), self.children[0])
-        scores = [Score(False, []) if s is None else s for s in scores]
-        if not scores:
-            raise ValueError
-
-        residues = set()
-
-        for s in scores:
-            residues |= s.residues
-            if not s.score:
-                return Score(False, [])
-
-        return Score(True, residues)
+        larg, rarg = self.children
+        return larg(mutations) and rarg(mutations)
 
 
 class OrExpr(AsiBinaryExpr):
     """Boolean OR on children (binary only)"""
 
     def __call__(self, mutations):
-        arg1, arg2 = self.children
-
-        score1 = arg1(mutations)
-        score2 = arg2(mutations)
-
-        if score1 is None:
-            score1 = Score(False, [])
-        if score2 is None:
-            score2 = Score(False, [])
-
-        return Score(score1.score or score2.score,
-                     score1.residues | score2.residues)
-
-
-class EqualityExpr(AsiExpr):
-    """ASI2 style inequality expressions"""
-
-    def __init__(self, label, pos, children):
-        super().__init__(label, pos, children)
-        self.operation, limit = children
-        self.limit = int(limit)
-
-    def __call__(self, x):
-        if self.operation == 'ATLEAST':
-            return x >= self.limit
-        elif self.operation == 'EXACTLY':
-            return x == self.limit
-        elif self.operation == 'NOMORETHAN':
-            return x <= self.limit
-
-        raise NotImplementedError
+        larg, rarg = self.children
+        return larg or rarg
 
 
 class ScoreExpr(AsiExpr):
     """Score expressions propagate DRM scores"""
 
     def __call__(self, mutations):
-
-        flags = {}
-        if len(self.children) == 4:
-            operation, _, flag, _ = self.children
-            flags[flag] = []
-            score = 0  # should be None
-
-        elif len(self.children) == 3:
-            operation, minus, score = self.children
-            if minus != '-':  # this is parsing the expression twice, refactor
-                raise ValueError
-            score = -1 * int(score)
-
-        elif len(self.children) == 2:
-            operation, score = self.children
-            score = int(score)
-
+        child = self.children[0]
+        if type(child) == ExprList:
+            return child(mutations)
         else:
-            raise ValueError
-
-        # evaluate operation and return score
-        result = operation(mutations)
-        if result is None:
-            return None
-
-        if result.score is False:
-            return Score(0, [])
-        return Score(score, result.residues, flags=flags)
+            return float(child)
 
 
-class ScoreList(AsiExpr):
-    """Lists of scores are either SUMed, MAXed, or MINed"""
+class ExprList(AsiExpr):
 
     def __call__(self, mutations):
         operation, *rest = self.children
@@ -180,51 +60,33 @@ class ScoreList(AsiExpr):
         elif operation == 'MIN':
             terms = rest
             func = min
+        elif operation == 'MEAN':
+            terms = rest
+            func = lambda x: sum(x) / len(x)
         else:
             # the default operation is sum
             terms = self.children
             func = sum
+
         scores = [f(mutations) for f in terms]
-        matched_scores = [score.score for score in scores if score.score]
-        residues = reduce(lambda x, y: x | y,
-                          (score.residues for score in scores))
-        flags = {}
-        for score in scores:
-            flags.update(score.flags)
-        return Score(bool(matched_scores) and func(matched_scores),
-                     residues,
-                     flags)
+
+        return func(scores)
 
 
-class SelectFrom(AsiExpr):
-    """Return True if some number of mutations match"""
-
-    def typecheck(self, tokens):
-        # if type(tokens[0]) != EqualityExpr:
-        #     raise TypeError()
-        pass
-
+class Condition(AsiExpr):
     def __call__(self, mutations):
-        operation, *rest = self.children
-        # the head of the arg list must be an equality expression
-       
-        scored = [f(mutations) for f in rest]
-        passing = sum(bool(score.score) for score in scored)
-
-        return Score(operation(passing),
-                     reduce(lambda x, y: x | y,
-                            (item.residues for item in scored)))
+        if len(self.children) == 1:
+            child = self.children[0]
+            return child(mutations)
 
 
-class AsiScoreCond(AsiExpr):
-    """Score condition"""
-
-    label = "ScoreCond"
-
-    def __call__(self, args):
-        """Score conditions evaluate a list of expressions and sum scores"""
-        return sum((f(args) for f in self.children), Score(False, set()))
-
+class Expr(AsiExpr):
+    def __call__(self, mutations):
+        condition, scoreExpr = self.children
+        if condition(mutations):
+            return scoreExpr(mutations)
+        else:
+            return 0.0
 
 class AsiMutations(object):
     """List of mutations given an ambiguous pattern"""
@@ -243,13 +105,13 @@ class AsiMutations(object):
             is_found |= mutation_set.pos == self.mutations.pos
             intersection = self.mutations.mutations & mutation_set.mutations
             if len(intersection) > 0:
-                return Score(True, intersection)
+                return True
 
         if not is_found:
             # Some required positions were not found in the environment.
             raise MissingPositionError('Missing position {}.'.format(
                 self.mutations.pos))
-        return Score(False, set())
+        return False
 
 
 class HCVR2(DRMParser):
@@ -272,7 +134,7 @@ class HCVR2(DRMParser):
         # complex atoms
         integer = Word(nums)
         residue = Optional(Regex(r'[A-Z]')) + integer + Regex(r'\!?[diA-Z]+')
-#        residue.setParseAction(AsiMutations)
+        residue.setParseAction(AsiMutations)
         
         float_ = Combine(Optional(Literal('-')) + integer +\
                          Optional(Literal('.') + Optional(integer)))
@@ -283,11 +145,10 @@ class HCVR2(DRMParser):
                  Literal('FALSE').suppress())
 
         # compound expressions
-
-       
         booleancondition = Forward()
 
         condition = residue | bool_ | l_par + booleancondition + r_par
+        condition.setParseAction(Condition)
 
         booleancondition << infixNotation(condition,
                                           [(and_, 2, opAssoc.LEFT, AndExpr),
@@ -295,12 +156,18 @@ class HCVR2(DRMParser):
 
         expr_list = Forward()
         score = float_ | expr_list
+        score.setParseAction(ScoreExpr)
 
         expr = condition + mapper + score
+        expr.setParseAction(Expr)
         expr_list << Optional(accumulator) + l_par + delimitedList(expr) + r_par
+        expr_list.setParseAction(ExprList)
 
         try:
             return score.parseString(rule)
         except ParseException as ex:
-            ex.msg = 'Error in HCVR: ' + ex.markInputline()
+            ex.msg = 'Error in HCVR2: ' + ex.markInputline()
             raise
+
+    def __call__(self, mutations):
+        return self.dtree(mutations)
--- pyvdrm/tests/test_hcvr2.py
+++ pyvdrm/tests/test_hcvr2.py
@@ -4,23 +4,34 @@ import unittest
 from pyparsing import ParseException
 
 from pyvdrm.drm import MissingPositionError
-from pyvdrm.hcvr2 import HCVR2, AsiMutations, Score
+from pyvdrm.hcvr2 import HCVR2, AsiMutations
 from pyvdrm.vcf import Mutation, MutationSet, VariantCalls
 
 from pyvdrm.tests.test_vcf import add_mutations
-
 
 # noinspection SqlNoDataSourceInspection,SqlDialectInspection
 class TestRuleParser(unittest.TestCase):
 
     def test_anything(self):
-        HCVR2("( 10N => -1.0 )")
+        r = HCVR2("( 10N => -1.0 )")
+        self.assertEqual(r(VariantCalls("10N")), -1.0)
+        self.assertEqual(r(VariantCalls("10X")), 0.0)
 
     def test_expr(self):
-        HCVR2("( 151M => -1.0, 130M => 2.0 )")
+        r = HCVR2("( 151M => -1.0, 130M => 2.0 )")
+        self.assertEqual(r(VariantCalls("151M 130M")), 1.0)
+        self.assertEqual(r(VariantCalls("151M 130T")), -1.0)
 
     def test_boolean_or(self):
-        HCVR2("( ( 151M AND 69i ) => 1.0)")
+        r = HCVR2("( ( 151M AND 69i ) => 1.0)")
+        self.assertEqual(r(VariantCalls("151M 69i")), 1.0)
+        self.assertEqual(r(VariantCalls("151S 69i")), 0.0)
 
     def test_nested_condition(self):
-        rule = HCVR2("(L41L => 3, 67N => (2N => 2)))")
+        r = HCVR2("(L41L => 3, 67N => (2N => 2)))")
+        self.assertEqual(r(VariantCalls("67N 2N L41S")), 2.0)
+
+    def test_mean_expr(self):
+        r = HCVR2("MEAN (L41L => 3, 67N => (2N => 2)))")
+        self.assertEqual(r(VariantCalls("67N 2N L41R")), 1.0)
+        self.assertEqual(r(VariantCalls("67N 2N L41L")), 2.5)
